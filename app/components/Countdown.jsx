825f77c221d43fb0cef3a56a7c61ea01f45986b0
Nitin96Bisht <nitin.bisht96@gmail.com>
2017-09-02 17:13:59 +0530

    Finalized Countdown

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -62,7 +62,7 @@ var Countdown = React.createClass({
 
     return (
       <div>
-        <h1 className="page-title">Countdown App</h1>
+        <h1 className="page-title">Countdown</h1>
         <Clock totalSeconds={count}/>
         {renderControlArea()}
       </div>
